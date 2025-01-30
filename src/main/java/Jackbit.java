0e1baccb9c056d897ca3565af9a72a8c86bbf063
NaelAnoob <116592348+NaelAnoob@users.noreply.github.com>
2025-01-31 04:45:55 +0800

    Level 0: Renamed, Greeted, Exited

---
+++ src/main/java/Jackbit.java
@@ -0,0 +1,31 @@
+public class Jackbit {
+    public static void main(String[] args) {
+        String box =    "       XXXXXXXXXXXXXXXXX        \n" +
+                        "     XXX     XX  XX    XX       \n" +
+                        "   XX        XX  XX     X       \n" +
+                        "   XX                   X       \n" +
+                        "    X        X    X     X       \n" +
+                        "    X        XXXXXX     X       \n" +
+                        "     X                 XX       \n" +
+                        "     XXXXXXXXXXXXXXXXXXX        \n" +
+                        "            XXXX                \n" +
+                        "          XXXX                  \n" +
+                        "           XXXX                 \n" +
+                        "            XXXXX               \n" +
+                        " XXX     XXXX             XXXX  \n" +
+                        "   XXX    XXXX          XXX     \n" +
+                        "    XXX     XXXX       XXX      \n" +
+                        "     XXXXXXXXXXXXXXXXXXXX       \n" +
+                        "     XXXXXXXXXXXXXXXXXXXX       \n" +
+                        "     XXXXXXXXXXXXXXXXXXXX       \n" +
+                        "     XXXXXXXXXXXXXXXXXXXX       \n" +
+                        "     XXXXXXXXXXXXXXXXXXXX       \n" +
+                        "     XXXXXXXXXXXXXXXXXXXX       \n" +
+                        "     XXXXXXXXXXXXXXXXXXXX       \n" +
+                        "     XXXXXXXXXXXXXXXXXXXX       \n" +
+                        "     XXXXXXXXXXXXXXXXXXXX       \n" +
+                        "     XXXXXXXXXXXXXXXXXXXX       ";
+
+        System.out.println("\n" + box + "\n \n POP!! I'm JackBit, but you can call me Jack! \n ________________________________ \n\n Have anything to talk about? \n ________________________________ \n\n See you later!!");
+    }
+}
