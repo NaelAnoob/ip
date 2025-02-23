150c663c36d93c853b640af4710ff1b796b6a7e1
NaelAnoob <116592348+NaelAnoob@users.noreply.github.com>
2025-02-23 22:51:27 +0800

    A-MoreOOP: Add Parser class

--- src/main/java/Jackbit.java
+++ src/main/java/Jackbit.java
@@ -123,11 +123,94 @@ public class Jackbit {
     //END OF TASKLIST//
 
 
-    //PARSER//
+    // PARSER //
 
+    private class Parser {
+        private TaskList taskList;
 
+        public Parser(TaskList taskList) {
+            this.taskList = taskList;
+        }
 
-    //END OF PARSER//
+        /**
+         * Parses the user command and performs the corresponding action.
+         *
+         * @param command The user command to parse.
+         */
+        public void parse(String command) throws JackbitException{
+            if (command.equals("list")) {
+                listTasks();
+            } else if (command.startsWith("mark")) {
+                markTask(command);
+            } else if (command.startsWith("unmark")) {
+                unmarkTask(command);
+            } else if (command.startsWith("delete")) {
+                deleteTask(command);
+            } else if (command.startsWith("todo") || command.startsWith("deadline") || command.startsWith("event")) {
+                addTask(command);
+            } else {
+                throw new JackbitException("First rule you learn in clown school: Random gibberish is never funny");
+            }
+        }
+
+        private void listTasks() {
+            System.out.println("Here are the tasks in your task_list: \n");
+            for (int i = 0; i < taskList.size(); i++) {
+                System.out.println((i + 1) + ". " + taskList.get(i));
+            }
+        }
+
+        private void markTask(String command) {
+            int index = Integer.parseInt(command.substring(5).trim()) - 1;
+            Task task = taskList.get(index);
+            task.mark();
+            System.out.println("Nice! I've marked this task as done: \n     " + task);
+        }
+
+        private void unmarkTask(String command) {
+            int index = Integer.parseInt(command.substring(7).trim()) - 1;
+            Task task = taskList.get(index);
+            task.unmark();
+            System.out.println("OK, I've marked this task as not done yet: \n     " + task);
+        }
+
+        private void deleteTask(String command) {
+            int index = Integer.parseInt(command.substring(7).trim()) - 1;
+            Task task = taskList.get(index);
+            taskList.remove(index);
+            System.out.println("I have removed this task from your task_list: \n" + task + "\n The number of tasks you have is " + taskList.size());
+        }
+
+        private void addTask(String command) throws JackbitException {
+            if (command.startsWith("todo")) {
+                if (command.length() < 6) {
+                    throw new JackbitException("You need to wind me up a little more bud. Give me more to work with and write your task fully.");
+                }
+                taskList.add(taskList.size(), new Todo(command.substring(5).trim()));
+            } else if (command.startsWith("deadline")) {
+                if (command.length() < 10) {
+                    throw new JackbitException("You need to wind me up a little more bud. Give me more to work with and write your task fully.");
+                }
+                String[] parts = command.substring(9).split("/by");
+                if (parts.length < 2) {
+                    throw new JackbitException("Invalid deadline format. Use: deadline <description> /by <date>");
+                }
+                taskList.add(taskList.size(), new Deadline(parts[0].trim(), parts[1].trim()));
+            } else if (command.startsWith("event")) {
+                if (command.length() < 7) {
+                    throw new JackbitException("You need to wind me up a little more bud. Give me more to work with and write your task fully.");
+                }
+                String[] parts = command.substring(6).split("/from|/to");
+                if (parts.length < 3) {
+                    throw new JackbitException("Invalid event format. Use: event <description> /from <start> /to <end>");
+                }
+                taskList.add(taskList.size(), new Event(parts[0].trim(), parts[1].trim(), parts[2].trim()));
+            }
+            System.out.println("I've added this task: \n" + taskList.get(taskList.size() - 1) + "\n The number of tasks you have is " + taskList.size());
+        }
+    }
+
+// END OF PARSER //
 
 
     //TASK//
@@ -269,10 +352,12 @@ public class Jackbit {
 
     }
 
+
+
     private Storage storage;
     private TaskList tasks;
     private Ui ui;
-
+    private Parser parser;
 
 
     public Jackbit(String filePath) {
@@ -283,6 +368,7 @@ public class Jackbit {
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         }
+        parser = new Parser(tasks);
     }
 
 
@@ -290,73 +376,20 @@ public class Jackbit {
         ui.welcome();
         Scanner chatter = new Scanner(System.in);
         String msg = chatter.nextLine();
-        int index = tasks.size();
-        Integer marker;
 
         while (!msg.equals("bye")){
 
-            if (msg.equals("list")) {
-                System.out.println("Here are the tasks in your task_list: \n");
-                for (int c = 1; c <= index; c++){
-                    System.out.println(c + "." + tasks.get(c - 1));
+            try {
+                parser.parse(msg);
+                msg = chatter.nextLine();
+            } catch (JackbitException e) {
+                try {
+                    storage.save(tasks.getTaskList());
+                } catch (IOException er) {
+                    throw new RuntimeException(e);
                 }
-            } else if (msg.startsWith("mark")) {
-                marker = Integer.valueOf(msg.substring(msg.length() - 1));
-                Task task = tasks.get(marker - 1);
-                task.mark();
-                System.out.println("Nice! I've marked this task as done: \n     " + task);
-
-            } else if (msg.startsWith("unmark")) {
-                marker = Integer.valueOf(msg.substring(msg.length() - 1));
-                Task task = tasks.get(marker - 1);
-                task.unmark();
-                System.out.println("OK, I've marked this task as not done yet: \n     " + task);
-            } else if (msg.startsWith("delete")) {
-                marker = Integer.valueOf(msg.substring(msg.length() - 1));
-                index--;
-
-                System.out.println("I have removed this task from your task_list: \n" + tasks.get(marker - 1) + "\n The number of tasks you have is " + index);
-                tasks.remove(marker.intValue() - 1);
-
+                throw new RuntimeException(e);
             }
-
-
-            else { //Adding to list
-                if (msg.startsWith("todo")) {
-                    if (msg.length() < 6) {
-                        try {
-                            throw new JackbitException("You need to wind me up a little more bud. Give me more to work with and write your task fully.");
-                        } catch (JackbitException e) {
-                            throw new RuntimeException(e);
-                        }
-                    }
-                    tasks.add(index, new Todo(msg));
-                } else if (msg.startsWith("deadline")) {
-                    String name = msg.substring(9,msg.indexOf("/by"));
-                    String by = msg.substring(msg.indexOf("/by") + 4);
-                    tasks.add(index, new Deadline(name, by));
-                } else if (msg.startsWith("event")) {
-                    String name = msg.substring(6,msg.indexOf("/from"));
-                    String from = msg.substring(msg.indexOf("/from") + 6, msg.indexOf("/to") - 1);
-                    String to = msg.substring(msg.indexOf("/to") + 4);
-                    tasks.add(index, new Event(name, from, to));
-                } else {
-                    try {
-                        throw new JackbitException("First rule you learn in clown school: Random gibberish is never funny");
-                    } catch (JackbitException e) {
-                        throw new RuntimeException(e);
-                    }
-                }
-
-
-                System.out.println("I've added this task: \n" + tasks.get(index) + "\n The number of tasks you have is " + (index + 1));
-
-                index++;
-
-
-            }
-
-            msg = chatter.nextLine();
 
         }
 
